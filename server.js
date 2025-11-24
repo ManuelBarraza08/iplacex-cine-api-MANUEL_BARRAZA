6772b18d37b0d68817452afaba06a04e50a4d251
ManuelBarraza08 <manuelbarrazac8@gmail.com>
2025-11-24 02:09:03 -0300

    Remove extended option from urlencoded middleware

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
