9e593e3fe5e24eb62402f490e167353eef0867ab
ManuelBarraza08 <manuelbarrazac8@gmail.com>
2025-11-24 02:12:13 -0300

    reparar post - get

--- src/actor/routes.js
+++ src/actor/routes.js
@@ -8,10 +8,12 @@ import {
 
 const actorRoutes = express.Router();
 
-actorRoutes.post("/actor", handleInsertActorRequest);
-actorRoutes.get("/actores", handleGetActoresRequest);
-actorRoutes.get("/actor/:id", handleGetActorByIdRequest);
-// Se adapta la ruta a /actor/pelicula/:peliculaId para evitar conflicto con /actor/:id
-actorRoutes.get("/actor/pelicula/:peliculaId", handleGetActoresByPeliculaRequest);
+actorRoutes.post("/", handleInsertActorRequest);
+
+actorRoutes.get("/", handleGetActoresRequest);
+
+actorRoutes.get("/:id", handleGetActorByIdRequest);
+
+actorRoutes.get("/pelicula/:peliculaId", handleGetActoresByPeliculaRequest);
 
 export default actorRoutes;
